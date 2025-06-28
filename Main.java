0ed194242f18dd19ce296651902b3f8a7befcadb
sakshij1 <sakshik27yt@gmail.com>
2025-06-28 19:56:20 +0530

    Inital commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+import java.util.Date;
+
+public class Main{
+    public static void main(String[] args){
+        System.out.println("Hello World !!");
+        System.out.println("THis is Jenkin tutorial");
+        System.out.println(new Date());
+        System.out.println("Hello");
+    }
+}
